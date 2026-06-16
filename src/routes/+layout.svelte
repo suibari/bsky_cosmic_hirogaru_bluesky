37f9422108d4042db6586e44f43cdcfc41602fe9
suibari <404.not.found.address@gmail.com>
2026-06-16 13:31:30 +0900

    favicon, OGP設定

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" type="image/png" href="/favicon.png" />
+	<meta property="og:title" content="超ひろがるBluesky" />
+	<meta property="og:description" content="Blueskyで無限に広がる青い宇宙!" />
+	<meta property="og:image" content="/ogp.png" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+	<meta property="og:type" content="website" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="超ひろがるBluesky" />
+	<meta name="twitter:description" content="Blueskyで無限に広がる青い宇宙!" />
+	<meta name="twitter:image" content="/ogp.png" />
+</svelte:head>
 {@render children()}
